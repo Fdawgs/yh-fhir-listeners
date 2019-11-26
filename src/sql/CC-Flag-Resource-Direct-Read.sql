f3f7ded6fdc480f332bb921a66d4d02a2320d31d
Frazer Smith <frazer.smith@ydh.nhs.uk>
2019-11-26 10:39:33 +0000

    Complete query

--- src/sql/CC-Flag-Resource-Direct-Read.sql
+++ src/sql/CC-Flag-Resource-Direct-Read.sql
@@ -1,24 +1,39 @@
-SELECT *
-FROM OPENQUERY(
-		[ENYH-PRD-ANALYTICS], 'SELECT DISTINCT TOP 100 *,
+WITH
+  flag_CTE
+  AS
+  (
+    SELECT DISTINCT flagId,
+      CASE ALM_Status
+      WHEN 'I' THEN 'Inactive'
+      WHEN 'A' THEN 'Active'
+      END AS flagStatus,
+      flagCategoryCodingDisplay,
+      flagCategoryCodingCode,
+      flagCodeCodingDisplay,
+      flagCodeCodingCode,
+      flagCodeText,
+      flagSubjectReference,
+      CONCAT(COALESCE(periodStartDate, ''),'T', COALESCE(periodStartTime, '')) AS periodStart,
+      CONCAT(COALESCE(periodEndDate, ''),'T00:00:00') AS periodEnd
+    FROM OPENQUERY(
+		[ENYH-PRD-ANALYTICS], 'SELECT DISTINCT TOP 100
+            ALM_Status,
             COALESCE(ALM_OnsetDate, ALM_CreateDate) AS periodStartDate,
             COALESCE(ALM_OnsetTime, ALM_CreateTime) AS periodStartTime,
             COALESCE(ALM_ExpiryDate, ALM_ClosedDate) AS periodEndDate,
-            -- IF 
             ALM_ClosedTime AS periodEndTime,        
             REPLACE(alert.ALM_RowID, ''||'', ''-'') AS flagId,
-            alert.ALM_Alert_DR->ALERT_Desc AS testy,
-            alert.ALM_AlertCategory_DR->ALERTCAT_Desc AS alert_type,
+            alert.ALM_Alert_DR->ALERT_Desc AS flagCodeCodingDisplay,
+            alert.ALM_Alert_DR->ALERT_Code AS flagCodeCodingCode,
+            alert.ALM_Message AS flagCodeText,
+            alert.ALM_AlertCategory_DR->ALERTCAT_Desc AS flagCategoryCodingDisplay,
+            alert.ALM_AlertCategory_DR->ALERTCAT_Code AS flagCategoryCodingCode,
             alert.ALM_PAPMI_ParRef->PAPMI_No AS flagSubjectReference
         FROM PA_AlertMsg alert
        WHERE alert.ALM_Alert_DR IS NOT NULL
-            AND ALM_ExpiryDate != ALM_ClosedDate
+         AND (alert.ALM_PAPMI_ParRef->PAPMI_No = ''5484125'')
          ')
-
+  )
 SELECT *
-FROM OPENQUERY(
-		[ENYH-PRD-ANALYTICS], 'SELECT DISTINCT ALERTCAT_Code,
-        ALERTCAT_Desc
-        FROM PAC_AlertCategory
-       WHERE ALERTCAT_DateTo IS NULL
-         ')
+FROM flag_CTE
+WHERE flagStatus IS NOT NULL;
