045a6dac85c73137cef519285311e1b86af43a17
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-01-07 12:25:01 +0000

    Add consultants, discharge, and admission details for IP encounters

--- src/sql/CC-Encounter-Resource-Direct-Read.sql
+++ src/sql/CC-Encounter-Resource-Direct-Read.sql
@@ -35,7 +35,13 @@ Apparently they use the discharge date in outpatients as the 'time all admin com
 Converting this to NULL.
 */
 
-WITH encounter_CTE(encounterIdentifier, encounterClassDesc, encounterClassCode, encounterTypeDesc, encounterTypeCode, encounterPeriodStartDate, encounterPeriodStartTime, encounterPeriodEndDate, encounterPeriodEndTime, subjectReference, encounterStatus, lastUpdateDate, lastUpdateTime)
+WITH encounter_CTE(encounterIdentifier, encounterClassDesc, encounterClassCode,
+	encounterTypeDesc, encounterTypeCode, encounterPeriodStartDate,
+	encounterPeriodStartTime, encounterPeriodEndDate, encounterPeriodEndTime,
+	encounterHospitalizationAdmitsourceCodingCode, encounterHospitalizationAdmitsourceCodingDesc, encounterHospitalizationDischargedispositionCodingCode,
+	encounterHospitalizationDischargedispositionCodingDesc, encounterAdmissionmethodCodingCode, encounterAdmissionmethodCodingDesc,
+	encounterDischargemethodCodingCode, encounterDischargemethodCodingDesc, subjectReference,
+	encounterStatus, lastUpdateDate, lastUpdateTime)
 AS (SELECT DISTINCT *
   	  FROM OPENQUERY([ENYH-PRD-ANALYTICS],
   				'SELECT REPLACE(app.APPT_RowId, ''||'', ''-'') AS encounterIdentifier,
@@ -49,6 +55,14 @@ AS (SELECT DISTINCT *
 						--app.APPT_DepartureTime AS encounterPeriodEndTime,
 						NULL AS encounterPeriodEndDate,
 						NULL AS encounterPeriodEndTime,
+						NULL AS encounterHospitalizationAdmitsourceCodingCode,
+						NULL AS encounterHospitalizationAdmitsourceCodingDesc,
+						NULL AS encounterHospitalizationDischargedispositionCodingCode,
+						NULL AS encounterHospitalizationDischargedispositionCodingDesc,
+						NULL AS encounterAdmissionmethodCodingCode,
+						NULL AS encounterAdmissionmethodCodingDesc,
+						NULL AS encounterDischargemethodCodingCode,
+						NULL AS encounterDischargemethodCodingDesc,
 						app.APPT_Adm_DR->PAADM_PAPMI_DR->PAPMI_No AS subjectReference,
 						app.APPT_Status AS encounterStatus,
 						app.APPT_LastUpdateDate AS lastUpdateDate,
@@ -74,6 +88,14 @@ AS (SELECT DISTINCT *
 						 PAADM_AdmTime AS encounterPeriodStartTime,
 						 PAADM_DischgDate AS encounterPeriodEndDate,
 						 PAADM_DischgTime AS encounterPeriodEndTime,
+						 PAADM_AdmSrc_DR->ADSOU_Code AS encounterHospitalizationAdmitsourceCodingCode,
+						 PAADM_AdmSrc_DR->ADSOU_Desc AS encounterHospitalizationAdmitsourceCodingDesc,
+						 PAADM_MainMRADM_DR->MRADM_DischDestin_DR->DDEST_Code AS encounterHospitalizationDischargedispositionCodingCode,
+						 PAADM_MainMRADM_DR->MRADM_DischDestin_DR->DDEST_Desc AS encounterHospitalizationDischargedispositionCodingDesc,
+						 PAADM_AdmMethod_DR->ADMETH_Code AS encounterAdmissionmethodCodingCode,
+						 PAADM_AdmMethod_DR->ADMETH_Desc AS encounterAdmissionmethodCodingDesc,
+						 PAADM_MainMRADM_DR->MRADM_ConditAtDisch_DR->DISCON_Code AS encounterDischargemethodCodingCode,
+						 PAADM_MainMRADM_DR->MRADM_ConditAtDisch_DR->DISCON_Desc AS encounterDischargemethodCodingDesc,
 						 PAADM_PAPMI_DR->PAPMI_No AS subjectReference,
 						 PAADM_VisitStatus AS encounterStatus,
 						 PAADM_UpdateDate AS lastUpdateDate,
@@ -109,6 +131,39 @@ SELECT  encounterIdentifier,
 		END AS encounterTypeCode,
 		CONCAT(COALESCE(encounterPeriodStartDate, ''), 'T', COALESCE(encounterPeriodStartTime, '')) AS encounterPeriodStart,
 		CONCAT(COALESCE(encounterPeriodEndDate, ''), 'T', COALESCE(encounterPeriodEndTime, '')) AS encounterPeriodEnd,
+		consultants.dischargeConsultantCode AS encounterParticipantIndividualCode_discharging,
+		consultants.dischargeConsultantDesc AS encounterParticipantIndividualDisplay_discharging,
+		consultants.admissionConsultantCode AS encounterParticipantIndividualCode_admitting,
+		consultants.admissionConsultantDesc AS encounterParticipantIndividualDesc_admitting,
+		encounterHospitalizationAdmitsourceCodingCode,
+		encounterHospitalizationAdmitsourceCodingDesc,
+		encounterHospitalizationDischargedispositionCodingCode,
+		encounterHospitalizationDischargedispositionCodingDesc,
+		encounterAdmissionmethodCodingCode,
+		encounterAdmissionmethodCodingDesc,
+		encounterDischargemethodCodingCode,
+		encounterDischargemethodCodingDesc,
 		subjectReference,
 		CONCAT(COALESCE(lastUpdateDate, ''), 'T', COALESCE(lastUpdateTime, '')) AS lastUpdated
-   FROM encounter_CTE;
+   FROM encounter_CTE
+		LEFT JOIN (SELECT *
+		   			 FROM (SELECT REPLACE(PAADM_ADMNo, '/', '-') AS PAADM_ADMNo,
+								  dischargeConsultantCode,
+								  dischargeConsultantDesc,
+								  admissionConsultantCode,
+								  admissionConsultantDesc,
+								  row_number() over (partition by PAADM_ADMNo order by TRANS_ChildSub)	AS transOrder
+							 FROM OPENQUERY([ENYH-PRD-ANALYTICS],
+							 		'SELECT TOP 100 TRANS_ParRef->PAADM_AdmDocCodeDR->CTPCP_Code AS dischargeConsultantCode,
+									 		TRANS_ParRef->PAADM_AdmDocCodeDR->CTPCP_Desc AS dischargeConsultantDesc,
+									 		TRANS_CTCP_DR->CTPCP_Code AS admissionConsultantCode,
+											TRANS_CTCP_DR->CTPCP_Desc AS admissionConsultantDesc,
+											TRANS_ChildSub,
+											TRANS_ParRef->PAADM_ADMNo
+									   FROM PA_AdmTransaction
+									  WHERE TRANS_ParRef->PAADM_Type = ''I''
+									  	AND TRANS_ParRef->PAADM_Epissubtype_DR->SUBT_Code = ''1''
+										AND TRANS_CTCP_DR IS NOT NULL
+                                        AND TRANS_ParRef->PAADM_PAPMI_DR->PAPMI_No = ''5484125''')
+						   ) a  WHERE transOrder = 1) consultants
+		ON encounter_CTE.encounterIdentifier = consultants.PAADM_ADMNo;
