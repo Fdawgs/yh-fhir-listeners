9556b0910d0ae9df4b2cc7b50a1b37f12686d693
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-01-08 10:09:18 +0000

    Format with Prettier

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-Yeovil District Hospital - SIDeR FHIR Listener Endpoints
-==========================================
-[![GitHub Release](https://img.shields.io/github/release/Fdawgs/ydh-fhir-listeners.svg)](https://github.com/Fdawgs/ydh-fhir-listeners/releases/latest/) [![Dependabot Status](https://api.dependabot.com/badges/status?host=github&identifier=217485645)](https://dependabot.com)
+# Yeovil District Hospital - SIDeR FHIR Listener Endpoints
+
+[![GitHub Release](https://img.shields.io/github/release/Fdawgs/ydh-fhir-listeners.svg)](https://github.com/Fdawgs/ydh-fhir-listeners/releases/latest/) [![Dependabot Status](https://api.dependabot.com/badges/status?host=github&identifier=217485645)](https://dependabot.com) [![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
 
 # Introduction
 
 ## Purpose
+
 This repo outlines the steps that have been taken to provide the technical deliverables required by the SIDeR programme, alongside the issues that were encountered during development, and how to deploy the resulting Mirth Connect channel.
 
 Work logs and issues are found in [docs/worklogs](https://github.com/Fdawgs/ydh-fhir-listeners/tree/master/docs/worklogs).
@@ -14,9 +15,11 @@ The intended audience for this page are team members of the Solutions Developmen
 This documentation has been written under the assumption that the reader has prior experience using Mirth Connect.
 
 ## Background
+
 [Somerset Clinical Commissioning Group](https://www.somersetccg.nhs.uk/#) (CCG) started the [SIDeR project](https://www.somersetccg.nhs.uk/your-health/sharing-your-information/sider/) with the purpose of linking up all main clinical and social care IT systems used in Somerset to improve and support direct care. [Black Pear Software Ltd.](https://www.blackpear.com/) (BP) is the technical partner that supports the project.
 
 Shareholders (as of 2019-04-03) are:
+
 - [Yeovil District Hospital NHS Foundation Trust](https://yeovilhospital.co.uk/) (YDH)
 - [Taunton and Somerset NHS Foundation Trust](https://www.tsft.nhs.uk/) (TST)
 - [Somerset Partnership NHS Foundation Trust](http://www.sompar.nhs.uk/) (SOMPAR)
@@ -28,33 +31,37 @@ Shareholders (as of 2019-04-03) are:
 - [Children's Hospice South West](https://www.chsw.org.uk/) (CHSW)
 - GP practices within Somerset (GPs)
 
-
 ## Deliverables
 
-###  Care Connect RESTful FHIR API endpoints
+### Care Connect RESTful FHIR API endpoints
+
 Black Pear have built a web front-end (eSP) for a shared care record, which will retrieve data relating to a patient from each shareholder that have the capability to do so, and amalgamate it into this record. The record is not stored in a cache anywhere and is built on the fly.
 Care providers can then access this record through a contextual link (an embedded link within the PAS).
 Black Pear need to be able to make GET requests to a RESTful FHIR API endpoints to retrieve a set of six FHIR resources that adhere to their respective [Care Connect API profiles](https://nhsconnect.github.io/CareConnectAPI/) to populate the record.
 
-###  Contextual Link
+### Contextual Link
+
 A contextual link needs to be added to our PAS to allow care providers access to the shared record. Refer to [Interoperability patterns - Contextual launch](https://github.com/Somerset-SIDeR-Programme/SIDeR-interop-patterns/wiki/contextual-launch) in the SIDeR wiki for more information.
 
 # Prerequisites
+
 - Latest Current/LTS version of [Node.js](https://nodejs.org/en/) (if you plan on using ESLint and other dev dependencies to extend it)
 - Latest release of [Mirth Connect](https://github.com/nextgenhealthcare/connect) installed (including supporting database instance)
 - Latest release of Mirth Connect's [FHIR Connector extension](https://ng.nextgen.com/l/488571/2018-03-16/6w3yr)
 - Latest release of [ydh-sider-authentication-service](https://github.com/Fdawgs/ydh-sider-authentication-service) (for securing endpoints with HTTPs and bearer tokens)
 
 # Deployment
+
 This Mirth Connect channel has been tested on a Mirth Connect instance (3.6 and greater) running on Windows 10 and Windows Server 2019, with an instance of SQL Server 2017 being used as the database backend.
 
 ## Setting up Mirth Connect Channel
+
 1. Ensure all prerequisites have been met, and you have a running instance of Mirth Connect
 2. Install the FHIR Connector extension from the file system
 3. Import the FHIR Listener channel from [src](https://github.com/Fdawgs/ydh-fhir-listeners/tree/master/src) into Mirth Connect
 4. Declare variables listed in the channel description, in the configuration map
 5. Deploy channel
 
-
 # License
+
 `ydh-fhir-listeners` is licensed under the [MIT](https://github.com/Fdawgs/ydh-fhir-listeners/blob/master/LICENSE) license.
--- src/mirth_code_templates/buildEncounterResource.js
+++ src/mirth_code_templates/buildEncounterResource.js
@@ -7,197 +7,234 @@
 	@return {Object} Encounter FHIR resource.
  */
 function buildEncounterResource(data) {
-	/**
-	 * Hard-coding meta profile and resourceType into resource as this should not
-	 * be changed for this resource, ever.
-	 */
-	var resource = {
-		meta: {
-			profile: [
-				'https://fhir.hl7.org.uk/STU3/StructureDefinition/CareConnect-Encounter-1'
-			]
-		},
-		resourceType: 'Encounter'
-	};
+  /**
+   * Hard-coding meta profile and resourceType into resource as this should not
+   * be changed for this resource, ever.
+   */
+  var resource = {
+    meta: {
+      profile: ['https://fhir.hl7.org.uk/STU3/StructureDefinition/CareConnect-Encounter-1']
+    },
+    resourceType: 'Encounter'
+  };
 
-	resource.id = newStringOrUndefined(getResultSetString(data, 'encounterIdentifier'));
-	resource.status = newStringOrUndefined(getResultSetString(data, 'encounterStatusMapped'));
+  resource.id = newStringOrUndefined(getResultSetString(data, 'encounterIdentifier'));
+  resource.status = newStringOrUndefined(getResultSetString(data, 'encounterStatusMapped'));
 
-	// Add meta data
-	if (getResultSetString(data, 'lastUpdated') != undefined
-		&& getResultSetString(data, 'lastUpdated').substring(0, 1) != 'T'
-		&& getResultSetString(data, 'lastUpdated').substring(0, 4) != '1900') {
-		resource.meta.lastUpdated = getResultSetString(data, 'lastUpdated');
-	}
+  // Add meta data
+  if (
+    getResultSetString(data, 'lastUpdated') != undefined &&
+    getResultSetString(data, 'lastUpdated').substring(0, 1) != 'T' &&
+    getResultSetString(data, 'lastUpdated').substring(0, 4) != '1900'
+  ) {
+    resource.meta.lastUpdated = getResultSetString(data, 'lastUpdated');
+  }
 
-	if (getResultSetString(data, 'encounterClassDesc') != undefined) {
-		resource.class = {
-			system: 'http://hl7.org/fhir/v3/ActEncounterCode',
-			code: newStringOrUndefined(getResultSetString(data, 'encounterClassCode')),
-			display: newStringOrUndefined(getResultSetString(data, 'encounterClassDesc'))
-		};
-	}
+  if (getResultSetString(data, 'encounterClassDesc') != undefined) {
+    resource.class = {
+      system: 'http://hl7.org/fhir/v3/ActEncounterCode',
+      code: newStringOrUndefined(getResultSetString(data, 'encounterClassCode')),
+      display: newStringOrUndefined(getResultSetString(data, 'encounterClassDesc'))
+    };
+  }
 
-	if (getResultSetString(data, 'encounterTypeCode') != undefined) {
-		resource.type = {
-			coding: [{
-				system: 'https://fhir.nhs.uk/STU3/CodeSystem/DCH-Specialty-1',
-				code: newStringOrUndefined(getResultSetString(data, 'encounterTypeCode')),
-				display: newStringOrUndefined(getResultSetString(data, 'encounterTypeDesc'))
-			}]
-		};
-	}
+  if (getResultSetString(data, 'encounterTypeCode') != undefined) {
+    resource.type = {
+      coding: [
+        {
+          system: 'https://fhir.nhs.uk/STU3/CodeSystem/DCH-Specialty-1',
+          code: newStringOrUndefined(getResultSetString(data, 'encounterTypeCode')),
+          display: newStringOrUndefined(getResultSetString(data, 'encounterTypeDesc'))
+        }
+      ]
+    };
+  }
 
-	// Add participants
-	resource.participant = [];
-	if (getResultSetString(data, 'encounterParticipantIndividualCode_admitting') != undefined
-		&& getResultSetString(data, 'encounterParticipantIndividualCode_discharging') != undefined
-		&& getResultSetString(data, 'encounterParticipantIndividualCode_discharging') == getResultSetString(data, 'encounterParticipantIndividualCode_admitting')) {
-		var participantCombo = {
-			type: [
-				{
-					coding: [
-						{
-							system: 'http://hl7.org/fhir/v3/ParticipationType',
-							code: 'ADM',
-							display: 'admitter'
-						}
-					]
-				},
-				{
-					coding: [
-						{
-							system: 'http://hl7.org/fhir/v3/ParticipationType',
-							code: 'DIS',
-							display: 'discharger'
-						}
-					]
-				}
-			],
-			individual: {
-				identifier: getResultSetString(data, 'encounterParticipantIndividualCode_admitting'),
-				display: getResultSetString(data, 'encounterParticipantIndividualDesc_admitting')
-			}
-		};
-		resource.participant.push(participantCombo);
-	} else if (getResultSetString(data, 'encounterParticipantIndividualCode_admitting') != undefined
-		&& getResultSetString(data, 'encounterParticipantIndividualDesc_admitting') != undefined) {
-		var participantAdmitter = {
-			type: [
-				{
-					coding: [
-						{
-							system: 'http://hl7.org/fhir/v3/ParticipationType',
-							code: 'ADM',
-							display: 'admitter'
-						}
-					]
-				}
-			],
-			individual: {
-				identifier: getResultSetString(data, 'encounterParticipantIndividualCode_admitting'),
-				display: getResultSetString(data, 'encounterParticipantIndividualDesc_admitting')
-			}
-		};
-		resource.participant.push(participantAdmitter);
-	} else if (getResultSetString(data, 'encounterParticipantIndividualCode_discharging') != undefined
-		&& getResultSetString(data, 'encounterParticipantIndividualDesc_discharging') != undefined) {
-		var participantDischarger = {
-			type: [
-				{
-					coding: [
-						{
-							system: 'http://hl7.org/fhir/v3/ParticipationType',
-							code: 'DIS',
-							display: 'discharger'
-						}
-					]
-				}
-			],
-			individual: {
-				identifier: getResultSetString(data, 'encounterParticipantIndividualCode_discharging'),
-				display: getResultSetString(data, 'encounterParticipantIndividualDesc_discharging')
-			}
-		};
-		resource.participant.push(participantDischarger);
-	}
+  // Add participants
+  resource.participant = [];
+  if (
+    getResultSetString(data, 'encounterParticipantIndividualCode_admitting') != undefined &&
+    getResultSetString(data, 'encounterParticipantIndividualCode_discharging') != undefined &&
+    getResultSetString(data, 'encounterParticipantIndividualCode_discharging') ==
+      getResultSetString(data, 'encounterParticipantIndividualCode_admitting')
+  ) {
+    var participantCombo = {
+      type: [
+        {
+          coding: [
+            {
+              system: 'http://hl7.org/fhir/v3/ParticipationType',
+              code: 'ADM',
+              display: 'admitter'
+            }
+          ]
+        },
+        {
+          coding: [
+            {
+              system: 'http://hl7.org/fhir/v3/ParticipationType',
+              code: 'DIS',
+              display: 'discharger'
+            }
+          ]
+        }
+      ],
+      individual: {
+        identifier: getResultSetString(data, 'encounterParticipantIndividualCode_admitting'),
+        display: getResultSetString(data, 'encounterParticipantIndividualDesc_admitting')
+      }
+    };
+    resource.participant.push(participantCombo);
+  } else if (
+    getResultSetString(data, 'encounterParticipantIndividualCode_admitting') != undefined &&
+    getResultSetString(data, 'encounterParticipantIndividualDesc_admitting') != undefined
+  ) {
+    var participantAdmitter = {
+      type: [
+        {
+          coding: [
+            {
+              system: 'http://hl7.org/fhir/v3/ParticipationType',
+              code: 'ADM',
+              display: 'admitter'
+            }
+          ]
+        }
+      ],
+      individual: {
+        identifier: getResultSetString(data, 'encounterParticipantIndividualCode_admitting'),
+        display: getResultSetString(data, 'encounterParticipantIndividualDesc_admitting')
+      }
+    };
+    resource.participant.push(participantAdmitter);
+  } else if (
+    getResultSetString(data, 'encounterParticipantIndividualCode_discharging') != undefined &&
+    getResultSetString(data, 'encounterParticipantIndividualDesc_discharging') != undefined
+  ) {
+    var participantDischarger = {
+      type: [
+        {
+          coding: [
+            {
+              system: 'http://hl7.org/fhir/v3/ParticipationType',
+              code: 'DIS',
+              display: 'discharger'
+            }
+          ]
+        }
+      ],
+      individual: {
+        identifier: getResultSetString(data, 'encounterParticipantIndividualCode_discharging'),
+        display: getResultSetString(data, 'encounterParticipantIndividualDesc_discharging')
+      }
+    };
+    resource.participant.push(participantDischarger);
+  }
 
-	resource.period = {};
-	if (getResultSetString(data, 'encounterPeriodStart') != undefined
-		&& getResultSetString(data, 'encounterPeriodStart').substring(0, 1) != 'T'
-		&& getResultSetString(data, 'encounterPeriodStart').substring(0, 4) != '1900') {
-		resource.period.start = getResultSetString(data, 'encounterPeriodStart');
-	}
-	if (getResultSetString(data, 'encounterPeriodEnd') != undefined
-		&& getResultSetString(data, 'encounterPeriodEnd').substring(0, 1) != 'T'
-		&& getResultSetString(data, 'encounterPeriodEnd').substring(0, 4) != '1900') {
-		resource.period.end = getResultSetString(data, 'encounterPeriodEnd');
-	}
+  resource.period = {};
+  if (
+    getResultSetString(data, 'encounterPeriodStart') != undefined &&
+    getResultSetString(data, 'encounterPeriodStart').substring(0, 1) != 'T' &&
+    getResultSetString(data, 'encounterPeriodStart').substring(0, 4) != '1900'
+  ) {
+    resource.period.start = getResultSetString(data, 'encounterPeriodStart');
+  }
+  if (
+    getResultSetString(data, 'encounterPeriodEnd') != undefined &&
+    getResultSetString(data, 'encounterPeriodEnd').substring(0, 1) != 'T' &&
+    getResultSetString(data, 'encounterPeriodEnd').substring(0, 4) != '1900'
+  ) {
+    resource.period.end = getResultSetString(data, 'encounterPeriodEnd');
+  }
 
-	// Add admission and discharge inpatient details
-	resource.hospitalization = {};
+  // Add admission and discharge inpatient details
+  resource.hospitalization = {};
 
-	if (getResultSetString(data, 'encounterAdmissionmethodCodingCode') != undefined
-	|| getResultSetString(data, 'encounterDischargemethodCodingCode') != undefined) {
-		resource.hospitalization.extension = [];
-	}
+  if (
+    getResultSetString(data, 'encounterAdmissionmethodCodingCode') != undefined ||
+    getResultSetString(data, 'encounterDischargemethodCodingCode') != undefined
+  ) {
+    resource.hospitalization.extension = [];
+  }
 
-	if (getResultSetString(data, 'encounterAdmissionmethodCodingCode') != undefined
-		&& getResultSetString(data, 'encounterAdmissionmethodCodingDesc') != undefined) {
-		var admissionMethod = {
-			url: newStringOrUndefined('https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect-AdmissionMethod-1'),
-			valueCodeableConcept: {
-				coding: [{
-					system: newStringOrUndefined('https://fhir.hl7.org.uk/STU3/ValueSet/CareConnect-AdmissionMethod-1'),
-					code: newStringOrUndefined(getResultSetString(data, 'encounterAdmissionmethodCodingCode')),
-					display: newStringOrUndefined(getResultSetString(data, 'encounterAdmissionmethodCodingDesc'))
+  if (
+    getResultSetString(data, 'encounterAdmissionmethodCodingCode') != undefined &&
+    getResultSetString(data, 'encounterAdmissionmethodCodingDesc') != undefined
+  ) {
+    var admissionMethod = {
+      url: newStringOrUndefined(
+        'https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect-AdmissionMethod-1'
+      ),
+      valueCodeableConcept: {
+        coding: [
+          {
+            system: newStringOrUndefined('https://fhir.hl7.org.uk/STU3/ValueSet/CareConnect-AdmissionMethod-1'),
+            code: newStringOrUndefined(getResultSetString(data, 'encounterAdmissionmethodCodingCode')),
+            display: newStringOrUndefined(getResultSetString(data, 'encounterAdmissionmethodCodingDesc'))
+          }
+        ]
+      }
+    };
+    resource.hospitalization.extension.push(admissionMethod);
+  }
 
-				}]
-			}
-		};
-		resource.hospitalization.extension.push(admissionMethod);
-	}
+  if (
+    getResultSetString(data, 'encounterDischargemethodCodingCode') != undefined &&
+    getResultSetString(data, 'encounterDischargemethodCodingDesc') != undefined
+  ) {
+    var dischargeMethod = {
+      url: newStringOrUndefined(
+        'https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect-DischargeMethod-1'
+      ),
+      valueCodeableConcept: {
+        coding: [
+          {
+            system: newStringOrUndefined('https://fhir.hl7.org.uk/STU3/ValueSet/CareConnect-DischargeMethod-1'),
+            code: newStringOrUndefined(getResultSetString(data, 'encounterDischargemethodCodingCode')),
+            display: newStringOrUndefined(getResultSetString(data, 'encounterDischargemethodCodingDesc'))
+          }
+        ]
+      }
+    };
+    resource.hospitalization.extension.push(dischargeMethod);
+  }
 
-	if (getResultSetString(data, 'encounterDischargemethodCodingCode') != undefined
-	&& getResultSetString(data, 'encounterDischargemethodCodingDesc') != undefined) {
-		var dischargeMethod = {
-			url: newStringOrUndefined('https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect-DischargeMethod-1'),
-			valueCodeableConcept: {
-				coding: [{
-					system: newStringOrUndefined('https://fhir.hl7.org.uk/STU3/ValueSet/CareConnect-DischargeMethod-1'),
-					code: newStringOrUndefined(getResultSetString(data, 'encounterDischargemethodCodingCode')),
-					display: newStringOrUndefined(getResultSetString(data, 'encounterDischargemethodCodingDesc'))
+  if (
+    getResultSetString(data, 'encounterHospitalizationAdmitsourceCodingCode') != undefined &&
+    getResultSetString(data, 'encounterHospitalizationAdmitsourceCodingDesc') != undefined
+  ) {
+    resource.hospitalization.admitSource = {
+      coding: [
+        {
+          system: newStringOrUndefined('https://fhir.hl7.org.uk/STU3/CodeSystem/CareConnect-SourceOfAdmission-1'),
+          code: newStringOrUndefined(getResultSetString(data, 'encounterHospitalizationAdmitsourceCodingCode')),
+          display: newStringOrUndefined(getResultSetString(data, 'encounterHospitalizationAdmitsourceCodingDesc'))
+        }
+      ]
+    };
+  }
+  if (
+    getResultSetString(data, 'encounterHospitalizationDischargedispositionCodingCode') != undefined &&
+    getResultSetString(data, 'encounterHospitalizationDischargedispositionCodingDesc') != undefined
+  ) {
+    resource.hospitalization.dischargeDisposition = {
+      coding: [
+        {
+          system: newStringOrUndefined('https://fhir.hl7.org.uk/STU3/CodeSystem/CareConnect-DischargeDestination-1'),
+          code: newStringOrUndefined(
+            getResultSetString(data, 'encounterHospitalizationDischargedispositionCodingCode')
+          ),
+          display: newStringOrUndefined(
+            getResultSetString(data, 'encounterHospitalizationDischargedispositionCodingDesc')
+          )
+        }
+      ]
+    };
+  }
 
-				}]
-			}
-		};
-		resource.hospitalization.extension.push(dischargeMethod);
-	}
+  resource.subject = {
+    reference: $cfg('apiUrl') + '/r3/Patient/' + getResultSetString(data, 'subjectReference')
+  };
 
-	if (getResultSetString(data, 'encounterHospitalizationAdmitsourceCodingCode') != undefined
-		&& getResultSetString(data, 'encounterHospitalizationAdmitsourceCodingDesc') != undefined) {
-		resource.hospitalization.admitSource = {
-			coding: [{
-				system: newStringOrUndefined('https://fhir.hl7.org.uk/STU3/CodeSystem/CareConnect-SourceOfAdmission-1'),
-				code: newStringOrUndefined(getResultSetString(data, 'encounterHospitalizationAdmitsourceCodingCode')),
-				display: newStringOrUndefined(getResultSetString(data, 'encounterHospitalizationAdmitsourceCodingDesc'))
-			}]
-		};
-	}
-	if (getResultSetString(data, 'encounterHospitalizationDischargedispositionCodingCode') != undefined
-		&& getResultSetString(data, 'encounterHospitalizationDischargedispositionCodingDesc') != undefined) {
-		resource.hospitalization.dischargeDisposition = {
-			coding: [{
-				system: newStringOrUndefined('https://fhir.hl7.org.uk/STU3/CodeSystem/CareConnect-DischargeDestination-1'),
-				code: newStringOrUndefined(getResultSetString(data, 'encounterHospitalizationDischargedispositionCodingCode')),
-				display: newStringOrUndefined(getResultSetString(data, 'encounterHospitalizationDischargedispositionCodingDesc'))
-			}]
-		};
-	}
-
-	resource.subject = {
-		reference: $cfg('apiUrl') + '/r3/Patient/' + getResultSetString(data, 'subjectReference')
-	};
-
-	return resource;
+  return resource;
 }
