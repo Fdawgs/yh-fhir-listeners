6e94be8cff003ecb636bfa5b788a31601ae68bdd
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-11-24 09:25:40 +0000

    refactor(code-templates): replace if... else chain with switch statement

--- dist/mirth_code_templates/getResultSet.js
+++ dist/mirth_code_templates/getResultSet.js
@@ -14,43 +14,66 @@ function getResultSet(resultSet) {
 	for (var index = 1; index < numColumns + 1; index++) {
 		var columnName = rsmd.getColumnName(index);
 
-		if (rsmd.getColumnType(index) == java.sql.Types.ARRAY) {
-			obj[''.concat(columnName)] = resultSet.getArray(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.BIGINT) {
-			obj[''.concat(columnName)] = resultSet.getInt(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.BOOLEAN) {
-			obj[''.concat(columnName)] = resultSet.getBoolean(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.BLOB) {
-			obj[''.concat(columnName)] = resultSet.getBlob(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.NCHAR) {
-			obj[''.concat(columnName)] = resultSet.getNString(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.CHAR) {
-			obj[''.concat(columnName)] = resultSet.getString(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.CLOB) {
-			obj[''.concat(columnName)] = getResultSetString(
-				resultSet,
-				columnName
-			);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.DOUBLE) {
-			obj[''.concat(columnName)] = resultSet.getDouble(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.FLOAT) {
-			obj[''.concat(columnName)] = resultSet.getFloat(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.INTEGER) {
-			obj[''.concat(columnName)] = resultSet.getInt(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.NVARCHAR) {
-			obj[''.concat(columnName)] = resultSet.getNString(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.VARCHAR) {
-			obj[''.concat(columnName)] = resultSet.getString(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.TINYINT) {
-			obj[''.concat(columnName)] = resultSet.getInt(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.SMALLINT) {
-			obj[''.concat(columnName)] = resultSet.getInt(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.DATE) {
-			obj[''.concat(columnName)] = resultSet.getDate(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.TIMESTAMP) {
-			obj[''.concat(columnName)] = resultSet.getTimestamp(columnName);
-		} else {
-			obj[''.concat(columnName)] = resultSet.getObject(columnName);
+		switch (rsmd.getColumnType(index)) {
+			case java.sql.Types.ARRAY:
+				obj[''.concat(columnName)] = resultSet.getArray(columnName);
+				break;
+			case java.sql.Types.BIGINT:
+				obj[''.concat(columnName)] = resultSet.getInt(columnName);
+				break;
+			case java.sql.Types.BLOB:
+				obj[''.concat(columnName)] = getResultSetString(
+					resultSet,
+					columnName
+				);
+
+				break;
+			case java.sql.Types.BOOLEAN:
+				obj[''.concat(columnName)] = resultSet.getBoolean(columnName);
+				break;
+			case java.sql.Types.CHAR:
+				obj[''.concat(columnName)] = resultSet.getString(columnName);
+				break;
+			case java.sql.Types.CLOB:
+				obj[''.concat(columnName)] = getResultSetString(
+					resultSet,
+					columnName
+				);
+
+				break;
+			case java.sql.Types.DATE:
+				obj[''.concat(columnName)] = resultSet.getDate(columnName);
+				break;
+			case java.sql.Types.DOUBLE:
+				obj[''.concat(columnName)] = resultSet.getDouble(columnName);
+				break;
+			case java.sql.Types.FLOAT:
+				obj[''.concat(columnName)] = resultSet.getFloat(columnName);
+				break;
+			case java.sql.Types.INTEGER:
+				obj[''.concat(columnName)] = resultSet.getInt(columnName);
+				break;
+			case java.sql.Types.NCHAR:
+				obj[''.concat(columnName)] = resultSet.getNString(columnName);
+				break;
+			case java.sql.Types.NVARCHAR:
+				obj[''.concat(columnName)] = resultSet.getNString(columnName);
+				break;
+			case java.sql.Types.SMALLINT:
+				obj[''.concat(columnName)] = resultSet.getInt(columnName);
+				break;
+			case java.sql.Types.TIMESTAMP:
+				obj[''.concat(columnName)] = resultSet.getTimestamp(columnName);
+				break;
+			case java.sql.Types.TINYINT:
+				obj[''.concat(columnName)] = resultSet.getInt(columnName);
+				break;
+			case java.sql.Types.VARCHAR:
+				obj[''.concat(columnName)] = resultSet.getString(columnName);
+				break;
+			default:
+				obj[''.concat(columnName)] = resultSet.getObject(columnName);
+				break;
 		}
 	}
 
--- src/mirth_code_templates/getResultSet.js
+++ src/mirth_code_templates/getResultSet.js
@@ -14,40 +14,64 @@ function getResultSet(resultSet) {
 	for (let index = 1; index < numColumns + 1; index++) {
 		const columnName = rsmd.getColumnName(index);
 
-		if (rsmd.getColumnType(index) == java.sql.Types.ARRAY) {
-			obj[`${columnName}`] = resultSet.getArray(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.BIGINT) {
-			obj[`${columnName}`] = resultSet.getInt(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.BOOLEAN) {
-			obj[`${columnName}`] = resultSet.getBoolean(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.BLOB) {
-			obj[`${columnName}`] = resultSet.getBlob(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.NCHAR) {
-			obj[`${columnName}`] = resultSet.getNString(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.CHAR) {
-			obj[`${columnName}`] = resultSet.getString(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.CLOB) {
-			obj[`${columnName}`] = getResultSetString(resultSet, columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.DOUBLE) {
-			obj[`${columnName}`] = resultSet.getDouble(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.FLOAT) {
-			obj[`${columnName}`] = resultSet.getFloat(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.INTEGER) {
-			obj[`${columnName}`] = resultSet.getInt(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.NVARCHAR) {
-			obj[`${columnName}`] = resultSet.getNString(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.VARCHAR) {
-			obj[`${columnName}`] = resultSet.getString(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.TINYINT) {
-			obj[`${columnName}`] = resultSet.getInt(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.SMALLINT) {
-			obj[`${columnName}`] = resultSet.getInt(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.DATE) {
-			obj[`${columnName}`] = resultSet.getDate(columnName);
-		} else if (rsmd.getColumnType(index) == java.sql.Types.TIMESTAMP) {
-			obj[`${columnName}`] = resultSet.getTimestamp(columnName);
-		} else {
-			obj[`${columnName}`] = resultSet.getObject(columnName);
+		switch (rsmd.getColumnType(index)) {
+			case java.sql.Types.ARRAY:
+				obj[`${columnName}`] = resultSet.getArray(columnName);
+				break;
+			case java.sql.Types.BIGINT:
+				obj[`${columnName}`] = resultSet.getInt(columnName);
+				break;
+			case java.sql.Types.BLOB:
+				obj[`${columnName}`] = getResultSetString(
+					resultSet,
+					columnName
+				);
+				break;
+			case java.sql.Types.BOOLEAN:
+				obj[`${columnName}`] = resultSet.getBoolean(columnName);
+				break;
+			case java.sql.Types.CHAR:
+				obj[`${columnName}`] = resultSet.getString(columnName);
+				break;
+			case java.sql.Types.CLOB:
+				obj[`${columnName}`] = getResultSetString(
+					resultSet,
+					columnName
+				);
+				break;
+			case java.sql.Types.DATE:
+				obj[`${columnName}`] = resultSet.getDate(columnName);
+				break;
+			case java.sql.Types.DOUBLE:
+				obj[`${columnName}`] = resultSet.getDouble(columnName);
+				break;
+			case java.sql.Types.FLOAT:
+				obj[`${columnName}`] = resultSet.getFloat(columnName);
+				break;
+			case java.sql.Types.INTEGER:
+				obj[`${columnName}`] = resultSet.getInt(columnName);
+				break;
+			case java.sql.Types.NCHAR:
+				obj[`${columnName}`] = resultSet.getNString(columnName);
+				break;
+			case java.sql.Types.NVARCHAR:
+				obj[`${columnName}`] = resultSet.getNString(columnName);
+				break;
+			case java.sql.Types.SMALLINT:
+				obj[`${columnName}`] = resultSet.getInt(columnName);
+				break;
+			case java.sql.Types.TIMESTAMP:
+				obj[`${columnName}`] = resultSet.getTimestamp(columnName);
+				break;
+			case java.sql.Types.TINYINT:
+				obj[`${columnName}`] = resultSet.getInt(columnName);
+				break;
+			case java.sql.Types.VARCHAR:
+				obj[`${columnName}`] = resultSet.getString(columnName);
+				break;
+			default:
+				obj[`${columnName}`] = resultSet.getObject(columnName);
+				break;
 		}
 	}
 
