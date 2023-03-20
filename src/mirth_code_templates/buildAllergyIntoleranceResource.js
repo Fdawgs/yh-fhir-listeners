1f73f3497e25e37557bb911c2c3ae55a0ef43293
Frazer Smith <frazer.dev@outlook.com>
2023-03-20 12:27:36 +0000

    refactor(allergyintolerance): object init

--- src/mirth_code_templates/buildAllergyIntoleranceResource.js
+++ src/mirth_code_templates/buildAllergyIntoleranceResource.js
@@ -29,80 +29,63 @@ function buildAllergyIntoleranceResource(data) {
 			profile: [
 				"https://fhir.hl7.org.uk/STU3/StructureDefinition/CareConnect-AllergyIntolerance-1",
 			],
+			tag: [
+				/**
+				 * Add SIDeR specific tags
+				 * Set tag to 'Do not Display' for all resources; decided by Paul Foster CCIO on 2020-11-19
+				 * due to low number of allergies recorded in TrakCare PAS
+				 */
+				{
+					system: "https://fhir.blackpear.com/ui/shared-care-record-visibility",
+					code: "none",
+					display: "Do not Display",
+				},
+			],
+			lastUpdated:
+				result.lastUpdated && !/^(T|1900)/m.test(result.lastUpdated)
+					? result.lastUpdated
+					: undefined,
 		},
 		resourceType: "AllergyIntolerance",
-	};
-
-	// Add meta data
-	if (
-		result.lastUpdated != undefined &&
-		result.lastUpdated.substring(0, 1) != "T" &&
-		result.lastUpdated.substring(0, 4) != "1900"
-	) {
-		resource.meta.lastUpdated = result.lastUpdated;
-	}
-
-	/**
-	 * Add SIDeR specific tags
-	 * Set tag to 'Do not Display' for all resources; decided by Paul Foster CCIO on 2020-11-19
-	 * due to low number of allergies recorded in TrakCare PAS
-	 */
-	resource.meta.tag = [
-		{
-			system: "https://fhir.blackpear.com/ui/shared-care-record-visibility",
-			code: "none",
-			display: "Do not Display",
+		id: newStringOrUndefined(result.id),
+		assertedDate: newStringOrUndefined(result.assertedDate),
+		code: {
+			coding: [],
 		},
-	];
-
-	resource.id = newStringOrUndefined(result.id);
-	resource.assertedDate = newStringOrUndefined(result.assertedDate);
-
-	resource.code = {
-		coding: [],
+		patient: {
+			reference: `${$cfg("apiUrl")}/STU3/Patient/${
+				result.patientReference
+			}`,
+		},
+		clinicalStatus: newStringOrUndefined(result.clinicalStatusCode),
+		verificationStatus: newStringOrUndefined(result.verificationStatusCode),
+		type: newStringOrUndefined(result.typeCode),
+		criticality: newStringOrUndefined(result.criticalityCode),
+		note: result.note ? [{ text: result.note.trim() }] : undefined,
 	};
 
-	if (result.allergyCodeCodingGroupCode != undefined) {
-		const groupCode = {
+	if (result.allergyCodeCodingGroupCode) {
+		resource.code.coding.push({
 			system: "https://trakcare.ydh.nhs.uk",
 			code: newStringOrUndefined(result.allergyCodeCodingGroupCode),
 			display: newStringOrUndefined(result.allergyCodeCodingGroupDisplay),
-		};
-		resource.code.coding.push(groupCode);
+		});
 	}
 
-	if (result.allergyCodeCodingCode != undefined) {
-		const allergyCode = {
+	if (result.allergyCodeCodingCode) {
+		resource.code.coding.push({
 			system: "https://trakcare.ydh.nhs.uk",
 			code: newStringOrUndefined(result.allergyCodeCodingCode),
 			display: newStringOrUndefined(result.allergyCodeCodingDisplay),
-		};
-		resource.code.coding.push(allergyCode);
+		});
 	}
 
-	if (result.allergyCodeCodingDrugCode != undefined) {
-		const drugCode = {
+	if (result.allergyCodeCodingDrugCode) {
+		resource.code.coding.push({
 			system: "https://trakcare.ydh.nhs.uk",
 			code: newStringOrUndefined(result.allergyCodeCodingDrugCode),
 			display: newStringOrUndefined(result.allergyCodeCodingDrugDisplay),
-		};
-		resource.code.coding.push(drugCode);
-	}
-
-	resource.patient = {
-		reference: `${$cfg("apiUrl")}/STU3/Patient/${result.patientReference}`,
-	};
-
-	resource.clinicalStatus = newStringOrUndefined(result.clinicalStatusCode);
-	resource.verificationStatus = newStringOrUndefined(
-		result.verificationStatusCode
-	);
-	resource.type = newStringOrUndefined(result.typeCode);
-	resource.criticality = newStringOrUndefined(result.criticalityCode);
-
-	// Add note
-	if (result.note != undefined) {
-		resource.note = [{ text: result.note.trim() }];
+		});
 	}
 
 	return resource;
